f4a92b7ce1597d47700e1467495a5aefedf97f74
elena <olenakunina@gmail.com>
2019-11-17 15:45:04 +0200

    mobile-menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var menuToggler = document.querySelector('.main-nav__toggler');
+var menuBlock = document.querySelector('.main-nav__list');
+
+menuToggler.addEventListener('click', function () {
+	menuBlock.classList.toggle('main-nav__list--shown');
+	menuToggler.classList.toggle('main-nav__toggler--close');
+});
