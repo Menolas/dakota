58ebeee647ecbd21feffefb52bd2a452c756ed7e
elena <olenakunina@gmail.com>
2019-11-19 22:24:36 +0200

    blog slider mobile js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,58 @@ menuToggler.addEventListener('click', function () {
 	menuBlock.classList.toggle('main-nav__list--shown');
 	menuToggler.classList.toggle('main-nav__toggler--close');
 });
+
+var blogHandlerForward = document.querySelector('.control-handlers__handler--right');
+var blogHandlerBack = document.querySelector('.control-handlers__handler--left');
+var blogArticles = document.querySelectorAll('.blog .blog__article');
+var findArticleIndex = function () {
+	for (var i = 0; i < blogArticles.length; i++) {
+		var index;
+		if (blogArticles[i].classList.contains('blog__article--shown')) {
+			index = i;
+		}
+	}
+	return index;
+};
+
+
+
+blogHandlerForward.addEventListener('click', function () {
+	if (!blogHandlerForward.classList.contains('control-handlers__handler--faded')) {
+	    var index = findArticleIndex();
+	    var nextArticleIndex = index + 1;
+
+	    if (nextArticleIndex > 0) {
+	    	blogHandlerBack.classList.remove('control-handlers__handler--faded');
+	    }
+
+	    if (index < blogArticles.length - 1) {
+			blogArticles[index].classList.remove('blog__article--shown');
+			blogArticles[nextArticleIndex].classList.add('blog__article--shown');
+
+			if (nextArticleIndex === blogArticles.length - 1) {
+			    blogHandlerForward.classList.add('control-handlers__handler--faded');
+		    }
+		}
+	}
+});
+
+blogHandlerBack.addEventListener('click', function () {
+	if (!blogHandlerBack.classList.contains('control-handlers__handler--faded')) {
+		var index = findArticleIndex();
+		var nextArticleIndex = index - 1;
+
+		if (nextArticleIndex < blogArticles.length - 1) {
+			blogHandlerForward.classList.remove('control-handlers__handler--faded');
+		}
+
+		if (index > 0) {
+			blogArticles[index].classList.remove('blog__article--shown');
+			blogArticles[nextArticleIndex].classList.add('blog__article--shown');
+
+			if (nextArticleIndex === 0) {
+				blogHandlerBack.classList.add('control-handlers__handler--faded');
+			}
+		}
+	}
+});
